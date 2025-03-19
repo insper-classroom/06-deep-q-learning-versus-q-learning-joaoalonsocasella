a7d27cd38951d88a60795c69f87e348846f1d30c
João Casella <j.casella2002@gmail.com>
2025-03-19 16:01:10 -0300

    modified:   FUNCTIONS/DeepQLearning.py modified:   FUNCTIONS/QLearning.py new file:   FUNCTIONS/__pycache__/DeepQLearning.cpython-312.pyc new file:   FUNCTIONS/__pycache__/QLearning.cpython-312.pyc renamed:    script/CartPole.py -> script/Cartpole/CartPole.py renamed:    script/CartPole_trained.py -> script/Cartpole/CartPole_trained.py renamed:    script/testando_modelos_tarefa06.py -> script/MountainCar_exec.py renamed:    script/treinando_modelos_tarefa06.py -> script/MountainCar_train.py

--- FUNCTIONS/DeepQLearning.py
+++ FUNCTIONS/DeepQLearning.py
@@ -1,19 +1,15 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
 import numpy as np
 import random
-import keras
-from keras import activations
-
 import gc
 
 class DeepQLearning:
-
-    #
-    # Implementacao do algoritmo proposto em 
-    # Playing Atari with Deep Reinforcement Learning, Mnih et al., 2013
-    # https://arxiv.org/abs/1312.5602
-    #
-
-    def __init__(self, env, gamma, epsilon, epsilon_min, epsilon_dec, episodes, batch_size, memory, model, max_steps):
+    def __init__(self,
+                 env, gamma, epsilon, epsilon_min,
+                 epsilon_dec, episodes, batch_size,
+                 memory, model, max_steps, device="cuda"):
         self.env = env
         self.gamma = gamma
         self.epsilon = epsilon
@@ -22,75 +18,87 @@ class DeepQLearning:
         self.episodes = episodes
         self.batch_size = batch_size
         self.memory = memory
-        self.model = model
+        self.model = model.to(device)
         self.max_steps = max_steps
+        self.device = device
+        self.optimizer = optim.Adam(self.model.parameters(), lr=0.001)
+        self.criterion = nn.MSELoss()
 
     def select_action(self, state):
         if np.random.rand() < self.epsilon:
             return random.randrange(self.env.action_space.n)
-        action = self.model.predict(state, verbose=0)
-        return np.argmax(action[0])
-
-    # cria uma memoria longa de experiencias
+        
+        state = torch.tensor(state, dtype=torch.float32, device=self.device).unsqueeze(0)
+        with torch.no_grad():
+            action_values = self.model(state)
+        return torch.argmax(action_values).item()
+    
     def experience(self, state, action, reward, next_state, terminal):
-        self.memory.append((state, action, reward, next_state, terminal)) 
-
+        self.memory.append((state, action, reward, next_state, terminal))
+    
     def experience_replay(self):
-        # soh acontece o treinamento depois da memoria ser maior que o batch_size informado
         if len(self.memory) > self.batch_size:
-            batch = random.sample(self.memory, self.batch_size) #escolha aleatoria dos exemplos
-            states = np.array([i[0] for i in batch])
-            actions = np.array([i[1] for i in batch])
-            rewards = np.array([i[2] for i in batch])
-            next_states = np.array([i[3] for i in batch])
-            terminals = np.array([i[4] for i in batch])
+            batch = random.sample(self.memory, self.batch_size)
 
-            # np.squeeze(): Remove single-dimensional entries from the shape of an array.
-            # Para se adequar ao input
-            states = np.squeeze(states)
-            next_states = np.squeeze(next_states)
+            states        = torch.tensor(np.array([i[0] for i in batch]), dtype=torch.float32, device=self.device)
+            actions       = torch.tensor(np.array([i[1] for i in batch]), dtype=torch.long, device=self.device)
+            rewards       = torch.tensor(np.array([i[2] for i in batch]), dtype=torch.float32, device=self.device)
+            next_states   = torch.tensor(np.array([i[3] for i in batch]), dtype=torch.float32, device=self.device)
+            terminals     = torch.tensor(np.array([i[4] for i in batch]), dtype=torch.float32, device=self.device)
 
-            # usando o modelo para selecionar as melhores acoes
-            next_max = np.amax(self.model.predict_on_batch(next_states), axis=1)
+            # Obter os valores Q estimados pela rede para os estados atuais
             
-            targets = rewards + self.gamma * (next_max) * (1 - terminals)
-            targets_full = self.model.predict_on_batch(states)
-            indexes = np.array([i for i in range(self.batch_size)])
-            
-            # usando os q-valores para atualizar os pesos da rede
-            targets_full[[indexes], [actions]] = targets
-            self.model.fit(states, targets_full, epochs=1, verbose=0)
-            
+            print(f"Shape de q_values antes do gather: {self.model(states).shape}")
+            print(f"Shape de actions antes do view: {actions.shape}")
+            print(f"Shape de actions após o view: {actions.view(-1, 1).shape}")
+            q_values = self.model(states).gather(1, actions.view(-1, 1)).squeeze(1)
+
+            # Calcular os valores Q esperados para os próximos estados (somente para estados não terminais)
+            with torch.no_grad():
+                next_max_q_values = self.model(next_states).max(1)[0]
+
+            # Atualizar os valores-alvo usando a equação de Bellman
+            targets = rewards + self.gamma * next_max_q_values * (1 - terminals.bool().float())
+
+            # Calcular a perda entre os valores Q previstos e os valores-alvo
+            loss = self.criterion(q_values, targets.detach())
+
+            # Backpropagation para atualizar os pesos da rede
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+
+            # Decaimento do epsilon para reduzir exploração com o tempo
             if self.epsilon > self.epsilon_min:
                 self.epsilon *= self.epsilon_dec
+
 
     def train(self):
         rewards = []
         for i in range(self.episodes+1):
-            (state,_) = self.env.reset()
+            state, _ = self.env.reset()
             state = np.reshape(state, (1, self.env.observation_space.shape[0]))
             score = 0
             steps = 0
-            #
-            # no caso do Cart Pole eh possivel usar a condicao de done do episodio
-            #
             done = False
+            
             while not done:
                 steps += 1
                 action = self.select_action(state)
                 next_state, reward, terminal, truncated, _ = self.env.step(action)
-                if terminal or truncated or (steps>self.max_steps):
+                if terminal or truncated or (steps > self.max_steps):
                     done = True          
                 score += reward
                 next_state = np.reshape(next_state, (1, self.env.observation_space.shape[0]))
                 self.experience(state, action, reward, next_state, terminal)
                 state = next_state
                 self.experience_replay()
+                
                 if done:
                     print(f'Episódio: {i+1}/{self.episodes}. Score: {score}')
                     break
+            
             rewards.append(score)
             gc.collect()
-            keras.backend.clear_session()
-
+        
         return rewards
---
+++ script/MountainCar_train.py
@@ -0,0 +1,120 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+import random
+import gymnasium as gym
+import gc
+import sys
+import csv
+from collections import deque
+
+path = r"C:/Users/João Alonso Casella/OneDrive/Joao/Insper/Eletivas/Reinforcement_Learning/06-deep-q-learning-versus-q-learning-joaoalonsocasella-1"
+sys.path.append(path)
+from FUNCTIONS.DeepQLearning import DeepQLearning as f_DQN
+####################
+import importlib
+import FUNCTIONS.DeepQLearning
+importlib.reload(FUNCTIONS.DeepQLearning)
+####################
+# Ambiente
+env = gym.make('MountainCar-v0', max_episode_steps=1000)
+np.random.seed(0)
+print('State space: ', env.observation_space)
+print('Action space: ', env.action_space)
+
+####################
+# Parâmetros
+gamma = 0.99
+epsilon = 1.0
+epsilon_min = 0.01
+epsilon_dec = 0.99
+episodes = 200
+batch_size = 64
+learning_rate = 0.001
+memory = deque(maxlen=10000)
+max_steps = 500
+N = 5
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+####################
+# Definição do modelo
+class DQN(nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(DQN, self).__init__()
+        self.fc1 = nn.Linear(input_dim, 512)
+        self.fc2 = nn.Linear(512, 256)
+        self.fc3 = nn.Linear(256, output_dim)
+
+    def forward(self, x):
+        if len(x.shape) == 1:
+            x = x.unsqueeze(0)
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x.squeeze(1)
+
+dqn_model  = DQN(env.observation_space.shape[0], env.action_space.n).to(device)
+optimizer = optim.Adam(dqn_model.parameters(), lr=learning_rate)
+criterion = nn.MSELoss()
+
+####################
+# Treinamento
+dqn_rewards = []
+
+for i in range(N):
+    print(f"Treinamento {i+1}/{N} - Deep Q-Learning")
+    dqn_agent  = f_DQN(env, gamma, epsilon,
+                epsilon_min, epsilon_dec,
+                episodes, batch_size, memory,
+                dqn_model , max_steps)
+    rewards_d = dqn_agent.train()
+    dqn_rewards.append(rewards_d)
+
+    # Salvar as recompensas do Deep Q-Learning em CSV
+    with open(f'results/mountaincar_DeepQLearning_rewards_run_{i}.csv', 'w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(["Episode", "Reward"])
+        for episode, reward in enumerate(rewards_d):
+            writer.writerow([episode, reward])
+
+# Salvar modelo treinado da rede neural
+torch.save(dqn_model.state_dict(), 'data/model_mountaincar.pth')
+
+# Plot comparativo
+import matplotlib.pyplot as plt
+plt.plot(np.mean(dqn_rewards, axis=0), label='Deep Q-Learning')
+plt.axhline(y=-110, color='r', linestyle='--', label='Meta de Recompensa')
+plt.xlabel('Episodes')
+plt.ylabel('Recompensa Acumulada')
+plt.legend()
+plt.title('Desempenho do Deep Q-Learning - MountainCar')
+plt.savefig("results/comparacao_dqn.jpg")
+plt.show()
+
+##############################################################################################################
+
+# Testar o modelo treinado
+env = gym.make('MountainCar-v0', render_mode='human').env
+(state, _) = env.reset()
+dqn_model.load_state_dict(torch.load('data/model_mountaincar.pth'))
+dqn_model.eval()
+
+done = False
+truncated = False
+rewards = 0
+steps = 0
+max_steps = 500
+
+while (not done) and (not truncated) and (steps < max_steps):
+    state_tensor = torch.tensor(state, dtype=torch.float32, device=device).unsqueeze(0)
+    with torch.no_grad():
+        Q_values = dqn_model(state_tensor)
+    action = torch.argmax(Q_values).item()
+    state, reward, done, truncated, info = env.step(action)
+    rewards += reward
+    env.render()
+    steps += 1
+
+print(f'Score = {rewards}')
+input('press a key...')
